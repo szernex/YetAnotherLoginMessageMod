67bb56e265c8bed08758b0a1f21cad1bd1b76335
Szernex <szernex@gmail.com>
2014-12-13 12:29:50 +0100

    Actual initial commit

---
+++ src/main/java/org/szernex/yalmm/Main.java
@@ -0,0 +1,9 @@
+package org.szernex.yalmm;
+
+import cpw.mods.fml.common.Mod;
+import org.szernex.yalmm.reference.Reference;
+
+@Mod(modid = Reference.MOD_ID, version = Reference.VERSION, name = Reference.MOD_NAME, /*guiFactory = Reference.GUI_FACTORY_CLASS,*/ acceptableRemoteVersions = "*")
+public class Main
+{
+}
---
+++ src/main/java/org/szernex/yalmm/reference/Reference.java
@@ -0,0 +1,10 @@
+package org.szernex.yalmm.reference;
+
+public class Reference
+{
+	public static final String MOD_ID = "YALMM";
+	public static final String MOD_NAME = "Yet Another Login Message Mod";
+	public static final String VERSION = "@VERSION@";
+
+	//public static final String GUI_FACTORY_CLASS = "org.szernex.yabm.client.gui.GuiFactory";
+}
